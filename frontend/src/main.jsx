9dc7153e77af9e3b93afff1126d81e43b9e01419
Stephan Granzow <stephangranzow@Mac.fritz.box>
2025-09-21 15:46:20 +0200

    frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from "react-dom/client";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import theme from "./theme/index.js";
+import queryClient from "./config/queryClient.js";
 
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ChakraProvider theme={theme}>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+        <ReactQueryDevtools position="bottom-right" initialIsOpen={false} />
+      </BrowserRouter>
+    </QueryClientProvider>
+  </ChakraProvider>
+);
